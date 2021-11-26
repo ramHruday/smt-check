9204c34cd25c7544085ee90c14163f6f9c1b1215
Hruday <ramahrudayb@gmail.com>
2021-11-26 01:08:31 -0600

    reenterncey emulated in 5.0

--- contracts/smtIntegerOverflow.sol
+++ contracts/smtIntegerOverflow.sol
@@ -1,36 +1,34 @@
-pragma solidity ^0.4.15;
+pragma solidity ^0.5.0;
 
 contract Reentrance {
     mapping(address => uint256) userBalance;
 
-    function getBalance(address u) constant returns (uint256) {
+    function getBalance(address u) public returns (uint256) {
         return userBalance[u];
     }
 
-    function addToBalance() payable {
+    function addToBalance() public payable {
         userBalance[msg.sender] += msg.value;
     }
 
-    function withdrawBalance() {
+    function withdrawBalance() public {
         // send userBalance[msg.sender] ethers to msg.sender
         // if mgs.sender is a contract, it will call its fallback function
-        if (!(msg.sender.call.value(userBalance[msg.sender])())) {
-            throw;
-        }
+        (bool success, ) = msg.sender.call.value(userBalance[msg.sender])("");
+        require(success);
         userBalance[msg.sender] = 0;
     }
 
-    function withdrawBalance_fixed() {
+    function withdrawBalance_fixed() public {
         // to protect against re-entrancy, the state variable
         // has to be change before the call
         uint256 amount = userBalance[msg.sender];
         userBalance[msg.sender] = 0;
-        if (!(msg.sender.call.value(amount)())) {
-            throw;
-        }
+        (bool success, ) = msg.sender.call.value(amount)("");
+        require(!success);
     }
 
-    function withdrawBalance_fixed_2() {
+    function withdrawBalance_fixed_2() public {
         // send() and transfer() are safe against reentrancy
         // they do not transfer the remaining gas
         // and they give just enough gas to execute few instructions
