929b8035cdb9672c2e0fff714aa6c0ffb827b813
Hruday <ramahrudayb@gmail.com>
2021-11-27 21:40:04 -0600

    smt overflow fixed

--- contracts/smtIntegerOverflow.sol
+++ contracts/smtIntegerOverflow.sol
@@ -1,17 +1,19 @@
-pragma solidity ^0.4.15;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+pragma experimental SMTChecker;
 
 contract Overflow {
-    uint private sellerBalance=0;
-    
-    function add(uint value) returns (bool){
+    uint256 private sellerBalance = 0;
+
+    function add(uint256 value) public returns (bool) {
         sellerBalance += value; // possible overflow
 
         // possible auditor assert
-        // assert(sellerBalance >= value); 
-    } 
+        // assert(sellerBalance >= value);
+    }
 
-    function safe_add(uint value) returns (bool){
+    function safe_add(uint256 value) public returns (bool) {
         require(value + sellerBalance >= sellerBalance);
-        sellerBalance += value; 
-    } 
+        sellerBalance += value;
+    }
 }
